ec656fa13aa05f94b922da712302b8233e1ac78f
Bogdan Zanozdra <bz300788@gmail.com>
2022-11-30 19:34:30 +0300

    Sem_5

--- task_4.py
+++ task_4.py
@@ -8,10 +8,50 @@ print(my_list)
 
 c = [int(i) for i in input('Введите позицию элемента через пробел ').split(' ')]
 print(c)
+
 prod = 1
 for i in range(len(c)):
     prod *= c[i]
 print(f'Произведение элементов на указанных через пробел позициях - {prod}')
 
+# Или:
 
-#
+n = int(input('Enter a number \n'))
+numbers = []
+for i in range(-n, n + 1):
+    numbers.append(i)
+print(f'Список элементов от -n до n: \n {numbers}')
+
+array_indexes = input('Enter indexes: ').split(' ')
+for i in range(len(array_indexes)):
+    array_indexes[i] = int(array_indexes[i])
+print(f'Список индексов для умножения: \n {array_indexes}')
+
+product = 1
+for i in array_indexes:
+    product *= numbers[i]
+print(f'Произведение элементов на указанных пользователем через пробел позициях: {product}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- task_5.py
+++ task_5.py
@@ -1,24 +1,36 @@
 # Реализуйте алгоритм перемешивания списка.
 
 import random 
+import time
 
 my_list = [1, 5, 3, 4, 7, 6]
 print(f'Исходный список: {my_list}')
-random.shuffle(my_list)
-print(my_list)
-def shuffle1(my_list):
 
-    new_list = list(range(len(my_list)))
+def shuffle1(my_list):
     for i in range(len(my_list)):
         rand_i = random.randint(0, len(my_list) - 1)
         temp = my_list[rand_i]
         my_list[rand_i] = my_list[i]
         my_list[i] = temp
     return my_list
-print(f' Перемешанный список: {shuffle1(my_list)}')
+print(f'Перемешанный список: {shuffle1(my_list)}')
 
-# Или так:
-# random.shuffle(my_list)
-# print(f'Перемешанный список: {my_list}')
+# # Или так:
 
+random.shuffle(my_list)
+print(f'Перемешанный список: {my_list}')
 
+# Или через тайм: 
+
+def shuffle_time(my_list):
+    for i in range(len(my_list)):    
+        rand_i = round(time.time())
+        rand_i = rand_i % 10
+        while rand_i > len(my_list):
+            rand_i = round(time.time())
+            rand_i = rand_i % 10
+        temp = my_list[rand_i]
+        my_list[rand_i] = my_list[i]
+        my_list[i] = temp
+    return my_list
+print(f'Перемешанный список: {shuffle_time(my_list)}')
